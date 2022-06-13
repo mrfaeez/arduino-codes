3eb3ccbebf99074157badf123bdff080b3ef651b
mrfaeez <faexd99@gmail.com>
2022-06-13 16:10:08 +0800

    add blynk codes

---
+++ Send_blynkk_notification/Send_blynkk_notification.ino
@@ -0,0 +1,51 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+//#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+int sensorPin = A0;   // Input pin for the Flame Sensor
+int sensorValue = 0;  // Variable to store the value coming from the flame sensor
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  delay(10);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  sensorValue = analogRead(sensorPin);
+  if (sensorValue < 700) {
+    Blynk.notify("Fire! Fire!");
+  }
+ 
+}
